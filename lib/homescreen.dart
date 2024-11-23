cbdd6b979af33df26aaef249788d8920eb078157
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-11-23 15:16:22 +0530

    snackbar in getx

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,10 +1,41 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Getx Tutorial',
+        ),
+      ),
+      body: Column(
+        children: [],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Get.snackbar(
+            'Message',
+            'Welcome',
+            duration: const Duration(
+              seconds: 2,
+            ),
+            colorText: Colors.white,
+            backgroundColor: Colors.blue,
+            snackPosition: SnackPosition.TOP,
+            icon: Icon(
+              Platform.isIOS ? Icons.message : Icons.message,
+              color: Colors.white,
+            ),
+          );
+        },
+      ),
+    );
   }
 }
