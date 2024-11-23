dfcc11f3a3c2d4db15d1bf744f99284fd17b5d6a
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-11-23 14:44:06 +0530

    getx added

---
+++ lib/setState.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class seStateExample extends StatefulWidget {
+  const seStateExample({super.key});
+
+  @override
+  State<seStateExample> createState() => _seStateExampleState();
+}
+
+class _seStateExampleState extends State<seStateExample> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
