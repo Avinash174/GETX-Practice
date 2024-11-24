b838fe083fe1f685a96de1b777f8d20ad2b04108
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-11-24 22:25:56 +0530

    new code

---
+++ lib/get_hw.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GetXHW extends StatefulWidget {
+  const GetXHW({super.key});
+
+  @override
+  State<GetXHW> createState() => _GetXHWState();
+}
+
+class _GetXHWState extends State<GetXHW> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
