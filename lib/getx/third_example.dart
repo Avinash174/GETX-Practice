965c7e4753e9931a6a8ea2441606b8b688cb7bb7
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-12-01 21:12:11 +0530

    third example of getx

---
+++ lib/getx/third_example.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ThirdExample extends StatefulWidget {
+  const ThirdExample({super.key});
+
+  @override
+  State<ThirdExample> createState() => _ThirdExampleState();
+}
+
+class _ThirdExampleState extends State<ThirdExample> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
 import 'package:getx_practice/getx/another_example.dart';
 import 'package:getx_practice/getx/example_two.dart';
+import 'package:getx_practice/getx/third_example.dart';
 import 'package:getx_practice/getx/without_getx.dart';
 import 'package:getx_practice/getx_hw.dart';
 import 'package:getx_practice/getx_local.dart';
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.light,
       debugShowCheckedModeBanner: false,
-      home: const AnotherExampleGetX(),
+      home: const ThirdExample(),
       locale: const Locale('en', 'US'),
       translations: Language(),
       fallbackLocale: const Locale('en', 'us'),
