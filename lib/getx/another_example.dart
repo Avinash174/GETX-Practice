fda1fe8f0fd269fa4314dbac3316834565dd38d6
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-12-01 20:21:03 +0530

    new code

---
+++ lib/getx/another_example.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AnotherExampleGetX extends StatefulWidget {
+  const AnotherExampleGetX({super.key});
+
+  @override
+  State<AnotherExampleGetX> createState() => _AnotherExampleGetXState();
+}
+
+class _AnotherExampleGetXState extends State<AnotherExampleGetX> {
+  @override
+  Widget build(BuildContext context) {
+    double opacity = .4;
+    @override
+    void initState() {
+      super.initState();
+    }
+
+    return Scaffold(
+      body: Column(),
+    );
+  }
+}
