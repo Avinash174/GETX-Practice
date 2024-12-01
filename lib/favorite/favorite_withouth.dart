35a0c83b52e24876426363ad5b6a5cec57b72f63
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-12-01 21:37:56 +0530

    new code

---
+++ lib/favorite/favorite_withouth.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FavoriteWithoutGetX extends StatefulWidget {
+  const FavoriteWithoutGetX({super.key});
+
+  @override
+  State<FavoriteWithoutGetX> createState() => _FavoriteWithoutGetXState();
+}
+
+class _FavoriteWithoutGetXState extends State<FavoriteWithoutGetX> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
