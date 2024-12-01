1b0a08873a7025426d506762c79dfedeb80fbfbd
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-12-01 21:22:47 +0530

    new changes done

--- lib/getx/third_example.dart
+++ lib/getx/third_example.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 class ThirdExample extends StatefulWidget {
@@ -8,8 +10,45 @@ class ThirdExample extends StatefulWidget {
 }
 
 class _ThirdExampleState extends State<ThirdExample> {
+  bool notification = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    log('build from heres');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Third Example Of GetX',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const Text(
+                'Notification',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Switch(
+                value: notification,
+                onChanged: (value) {
+                  setState(() {
+                    notification = value;
+                  });
+                },
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
