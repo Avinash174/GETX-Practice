144adaced45707a082819342707fa1bfb50cb932
Avinash Magar <robinw@recipto.in>
2024-12-06 20:00:25 +0530

    new code

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
-import 'package:getx_practice/favorite/favorite_withgetx.dart';
-import 'package:getx_practice/favorite/favorite_withouth.dart';
 import 'package:getx_practice/getx/another_example.dart';
-import 'package:getx_practice/getx/example_two.dart';
-import 'package:getx_practice/getx/third_example.dart';
-import 'package:getx_practice/getx/without_getx.dart';
 import 'package:getx_practice/getx_hw.dart';
 import 'package:getx_practice/getx_local.dart';
 import 'package:getx_practice/homescreen.dart';
-import 'package:getx_practice/image_picker/image_picker.dart';
 import 'package:getx_practice/language.dart';
 import 'package:getx_practice/navigation.dart';
 import 'package:getx_practice/second_page.dart';
