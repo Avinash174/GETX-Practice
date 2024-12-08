1a2a2a3074ba588fef9d9b127d43417427f8a68d
Avinash Magar <robinw@recipto.in>
2024-12-08 17:52:04 +0530

    new code

---
+++ lib/login_getx/login_controller.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart';
+
+class LogInController extends GetxController {
+  final emailController = TextEditingController().obs;
+  final passwordController = TextEditingController().obs;
+
+  void loginApi() async {
+    try {
+      final response =
+          await post(Uri.parse('https://reqres.in/api/login'), body: {
+        'email': emailController.value.text,
+        'password': passwordController.value.text,
+      });
+      var data = jsonDecode(response.body);
+      log('$response.statusCode');
+      log('$data');
+      if (response.statusCode == 200) {
+        Get.snackbar('Login Sucessful', data['error']);
+      } else {
+        Get.snackbar(
+          'Login Failed',
+          data['error'],
+        );
+      }
+    } catch (e) {
+      Get.snackbar('Execption', e.toString());
+    }
+  }
+}
--- lib/login_getx/login_getx.dart
+++ lib/login_getx/login_getx.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_practice/login_getx/login_controller.dart';
 
 class LogInWithGetX extends StatefulWidget {
   const LogInWithGetX({super.key});
@@ -8,6 +10,7 @@ class LogInWithGetX extends StatefulWidget {
 }
 
 class _LogInWithGetXState extends State<LogInWithGetX> {
+  LogInController controller = Get.put(LogInController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,8 +18,49 @@ class _LogInWithGetXState extends State<LogInWithGetX> {
         backgroundColor: Colors.orangeAccent,
         title: const Text('Log In GetX'),
       ),
-      body: Column(
-        children: [],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: controller.emailController.value,
+              decoration: const InputDecoration(
+                hintText: 'Enter Email Adderess',
+              ),
+            ),
+            SizedBox(
+              height: Get.height * .025,
+            ),
+            TextFormField(
+              controller: controller.passwordController.value,
+              decoration: const InputDecoration(
+                hintText: 'Enter Password',
+              ),
+            ),
+            SizedBox(
+              height: Get.height * .04,
+            ),
+            InkWell(
+              onTap: () {
+                controller.loginApi();
+              },
+              child: Container(
+                height: Get.height * .05,
+                color: Colors.orangeAccent,
+                child: const Center(
+                  child: Text(
+                    'Log In',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
